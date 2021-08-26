7d2add0d3e0915e4fd4a21bc22ed0e95c7ab369e
meg <maggiekendra12@gmail.com>
2021-08-26 07:53:12 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:aunty_kat_recipe_app/Pages/LoadingPage.dart';
+import 'package:aunty_kat_recipe_app/screens/home.dart';
+import 'package:aunty_kat_recipe_app/screens/splashScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget page = LoadingPage();
+  final storage = FlutterSecureStorage();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkLogin();
+  }
+
+  void checkLogin() async {
+    String token = await storage.read(key: "token");
+    if (token != null) {
+      setState(() {
+        page = HomeScreen();
+      });
+    } else {
+      setState(() {
+        page = SplashScreen();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: page,
+    );
+  }
+}
